479ef2b463b9f90579ab2fb41856ccb881e732fd
Luis Ruediger <luisruediger27@gmail.com>
2024-06-25 14:20:39 -0300

    Add user table

--- src/main/java/com/luisruediger/todolist/user/UserModel.java
+++ src/main/java/com/luisruediger/todolist/user/UserModel.java
@@ -1,11 +1,28 @@
 package com.luisruediger.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity(name = "tb_users")
 public class UserModel {
+
+  @Id
+  @Generated(value = "UUID")
+  private UUID id;
   
   private String username;
   private String name;
   private String password;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+  
 }

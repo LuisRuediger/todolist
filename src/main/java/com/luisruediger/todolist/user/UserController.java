703ff2f42e9de5f162e11bfc67cb2dc6a15f2b75
Luis Ruediger <luisruediger27@gmail.com>
2024-06-25 17:41:42 -0300

    add response code with ResponseEntity

--- src/main/java/com/luisruediger/todolist/user/UserController.java
+++ src/main/java/com/luisruediger/todolist/user/UserController.java
@@ -1,6 +1,8 @@
 package com.luisruediger.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,15 +17,15 @@ public class UserController {
   private IUserRepository userRepository;
 
   @PostMapping("/")
-  public UserModel create(@RequestBody UserModel userModel) {
+  // ResponsyEntity usado para retornar status code
+  public ResponseEntity create(@RequestBody UserModel userModel) {
     var user = this.userRepository.findByUsername(userModel.getUsername());
 
     if (user != null) {
-      System.out.println("usuário já existe");
-      return null;
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já cadastrado");
     }
     
     var userCreated = this.userRepository.save(userModel);
-    return userCreated;
+    return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
   }
 }

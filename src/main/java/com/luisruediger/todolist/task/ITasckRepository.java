01425697ce2ce27195bb742ed51c7a41f5f0c611
Luis Ruediger <luisruediger27@gmail.com>
2024-07-22 20:02:45 -0300

    listando tarefas

--- src/main/java/com/luisruediger/todolist/task/ITasckRepository.java
+++ src/main/java/com/luisruediger/todolist/task/ITasckRepository.java
@@ -3,7 +3,9 @@ package com.luisruediger.todolist.task;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
 
 public interface ITasckRepository extends JpaRepository<TaskModel, UUID> {
-  
+  List<TaskModel> findByIdUser(UUID idUser);
 }

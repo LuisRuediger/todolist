4dc76da269db87c55c23c637de0480eecbb7096e
Luis Ruediger <luisruediger27@gmail.com>
2024-07-23 17:55:08 -0300

    update parcial

--- src/main/java/com/luisruediger/todolist/task/TaskController.java
+++ src/main/java/com/luisruediger/todolist/task/TaskController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.luisruediger.todolist.utils.Utils;
+
 import jakarta.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -54,9 +56,10 @@ public class TaskController {
 
   @PutMapping("/{id}")
   public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
-    var idUser = request.getAttribute("idUser");
-    taskModel.setIdUser((UUID) idUser);
-    taskModel.setId(id);
-    return this.taskRepository.save(taskModel);
+    var task = this.taskRepository.findById(id).orElse(null);
+
+    Utils.copyNonNullProperies(taskModel, task);
+
+    return this.taskRepository.save(task);
   }
 }

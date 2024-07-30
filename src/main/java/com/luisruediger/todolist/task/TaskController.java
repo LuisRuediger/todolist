b8275bb8c3bcf5304192e3c4d10e418f9c6fd17d
Luis Ruediger <luisruediger27@gmail.com>
2024-07-30 17:54:29 -0300

    validando usuario dono

--- src/main/java/com/luisruediger/todolist/task/TaskController.java
+++ src/main/java/com/luisruediger/todolist/task/TaskController.java
@@ -55,11 +55,22 @@ public class TaskController {
   }
 
   @PutMapping("/{id}")
-  public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+  public ResponseEntity update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
     var task = this.taskRepository.findById(id).orElse(null);
 
-    Utils.copyNonNullProperies(taskModel, task);
+    if(task == null) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Tarefa não econtrada");
+    }
 
-    return this.taskRepository.save(task);
+    var idUser = request.getAttribute("idUser");
+
+    if(!task.getIdUser().equals(idUser)) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("O usuário não tem permissão para alterar a tarefa");
+    }
+
+    
+    Utils.copyNonNullProperies(taskModel, task);
+    var taskUpdated = this.taskRepository.save(task);
+    return ResponseEntity.ok().body(taskUpdated);
   }
 }
